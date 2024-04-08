81564b91cb97f431720e435ed20140684ef64fc9
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-04-08 20:56:40 +0200

    Change arrow-left folder location

--- src/components/return_button/Return_button.jsx
+++ src/components/return_button/Return_button.jsx
@@ -7,7 +7,7 @@ function ReturnButton({onClick}) {
 
     return (
         <button key={uuid()} type='button' className={style.button} onClick={onClick}>
-            <img className={style.icon} src='src/assets/arrow-left-solid.svg' alt='arrow back'/>
+            <img className={style.icon} src='/arrow-left-solid.svg' alt='arrow back'/>
             <p>Back</p>
         </button>
     )
